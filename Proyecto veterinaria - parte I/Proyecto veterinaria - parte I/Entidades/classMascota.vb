d1e1c21ea0015fd0968e30917f6f2a829db0c524
Dennis Valdes <dennis.valdez@anima.edu.uy>
2020-07-02 11:05:54 -0300

    modified:   Proyecto veterinaria - parte I/Proyecto veterinaria - parte I/Entidades/classMascota.vb 	modified:   Proyecto veterinaria - parte I/Proyecto veterinaria - parte I/Entidades/classPersona.vb

--- Proyecto veterinaria - parte I/Proyecto veterinaria - parte I/Entidades/classMascota.vb
+++ Proyecto veterinaria - parte I/Proyecto veterinaria - parte I/Entidades/classMascota.vb
@@ -1,15 +1,43 @@
 ﻿Public Class classMascota
-    Private name_ As String
-    Private añoNacimiento_ As Integer
-    Private ciDueño_ As Integer
+    Private _name As String
+    Private _añoNacimiento As Integer
+    Private _ciDueño As Integer
 
     Public Sub altaMascota(name, añoNacimiento, ciDueño)
-        name = Me.name_
-        añoNacimiento = Me.añoNacimiento_
-        ciDueño = Me.ciDueño_
+        name = Me._name
+        añoNacimiento = Me._añoNacimiento
+        ciDueño = Me._ciDueño
     End Sub
 
     Public Sub altaMascota()
 
     End Sub
+
+    Public Property name As String
+        Get
+            Return _name
+        End Get
+        Set(value As String)
+            _name = value
+        End Set
+    End Property
+
+    Public Property añoNacimiento As Integer
+        Get
+            Return _añoNacimiento
+        End Get
+        Set(value As Integer)
+            _añoNacimiento = value
+        End Set
+    End Property
+
+    Public Property ciDueño As Integer
+        Get
+            Return _ciDueño
+        End Get
+        Set(value As Integer)
+            _ciDueño = value
+        End Set
+    End Property
+
 End Class
--- Proyecto veterinaria - parte I/Proyecto veterinaria - parte I/Entidades/classPersona.vb
+++ Proyecto veterinaria - parte I/Proyecto veterinaria - parte I/Entidades/classPersona.vb
@@ -1,17 +1,54 @@
 ﻿Public Class classPersona
-    Private name_ As String
-    Private ci_ As Integer
-    Private dir_ As String
-    Private tel_ As Integer
+    Private _name As String
+    Private _ci As Integer
+    Private _dir As String
+    Private _tel As Integer
 
     Public Sub altaPersona(ci, name, dir, tel)
-        ci = Me.ci_
-        name = Me.name_
-        dir = Me.dir_
-        tel = Me.tel_
+        ci = Me._ci
+        name = Me._name
+        dir = Me._dir
+        tel = Me._tel
     End Sub
 
     Public Sub altaPersona()
 
     End Sub
+
+    Public Property name As String
+        Get
+            Return _name
+        End Get
+        Set(value As String)
+            _name = value
+        End Set
+    End Property
+
+    Public Property ci As Integer
+        Get
+            Return _ci
+        End Get
+        Set(value As Integer)
+            _ci = value
+        End Set
+    End Property
+
+    Public Property dir As String
+        Get
+            Return _dir
+        End Get
+        Set(value As String)
+            _dir = value
+        End Set
+    End Property
+
+    Public Property tel As Integer
+        Get
+            Return _tel
+        End Get
+        Set(value As Integer)
+            _tel = value
+        End Set
+    End Property
+
 End Class
