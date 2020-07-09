63135345bcb5b8455d08dae75c9655067014488b
DennisValdes <49279458+DennisVAldes@users.noreply.github.com>
2020-07-09 11:32:58 -0300

    Update conexion.vb

--- Proyecto veterinaria - parte I/Proyecto veterinaria - parte I/Persistencia/conexion.vb
+++ Proyecto veterinaria - parte I/Proyecto veterinaria - parte I/Persistencia/conexion.vb
@@ -1,5 +1,18 @@
 Public Class conexion
 
-    Public Function initCon() As Npgsql
+    Public Function initCon() As Npgsql.NpgsqlConnection
+        Dim conn as Npgsql.NpgsqlConnection
+        Try
+            conn = New Npgsql.NpgsqlConnection()
+            Dim cadenaDeComandos As String
+            
+            conn.ConnectionString = cadenaDeComandos
+            conn.Open()
+            
+        Catch ex as exception
+            throw ex
+            
+        End Try
+    End Function
 
 End Class
