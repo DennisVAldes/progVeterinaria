f5fc7f08f5a4c201cbf1b611848318af8d5205e6
DennisValdes <dennis@decemberlabs.com>
2020-09-03 16:23:53 -0300

    Fixes

--- Proyecto veterinaria - parte I/Proyecto veterinaria - parte I/Persistencia/persistenciaPersona.vb
+++ Proyecto veterinaria - parte I/Proyecto veterinaria - parte I/Persistencia/persistenciaPersona.vb
@@ -103,4 +103,43 @@ Public Class persistenciaPersona
         Return list
     End Function
 
+    Public Sub updatePersona(newPersona As classPersona)
+        Try
+            Dim conn As New Conexion
+            localConn = conn.InitConn
+            Dim cadenaDeComandos As String
+
+            cadenaDeComandos = "UPDATE TABLE persona(ci, nombre, direccion) values (@ci, @nombre, @direccion);"
+
+            Dim cmd As New Npgsql.NpgsqlCommand
+            cmd.CommandText = cadenaDeComandos
+            cmd.Connection = localConn
+
+            cmd.Parameters.Add("@ci", NpgsqlTypes.NpgsqlDbType.Integer).Value = newPersona.Ci
+            cmd.Parameters.Add("@nombre", NpgsqlTypes.NpgsqlDbType.Varchar, 100).Value = newPersona.Name
+            cmd.Parameters.Add("@direccion", NpgsqlTypes.NpgsqlDbType.Varchar, 100).Value = newPersona.Dir
+
+            Dim res As Integer
+            res = cmd.ExecuteNonQuery()
+
+            If res = 1 Then
+                Dim i = 0
+                While i < newPersona.Telefonos.Count
+
+                    cadenaDeComandos = "insert into telefono (personaci, telefono) values (@ci, @telefono)"
+                    cmd.CommandText = cadenaDeComandos
+                    cmd.Parameters.Add("@ci", NpgsqlTypes.NpgsqlDbType.Integer).Value = newPersona.Ci
+                    cmd.Parameters.Add("@telefono", NpgsqlTypes.NpgsqlDbType.Integer).Value = newPersona.Telefonos.Item(i)
+
+                    res = cmd.ExecuteNonQuery()
+
+                    i += 1
+                End While
+            End If
+
+        Catch ex As Exception
+            Throw ex
+
+        End Try
+    End Sub
 End Class
